fe4e9593bb3569c625e5247a4ec7e6d35780559e
itscatus <felicianatania11@gmail.com>
2025-05-30 21:31:43 +0700

    tes

--- src/navigation/NavbarTwoColumns.tsx
+++ src/navigation/NavbarTwoColumns.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import type { ReactNode } from 'react';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 type INavbarProps = {
   logo: ReactNode;
